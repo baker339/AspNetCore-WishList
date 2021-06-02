edd83deab58e0153af44f692577745c05aff31e1
Aidan Baker <baker339@purdue.edu>
2021-06-02 14:43:04 -0400

    Added item controller

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        
+        
+        // GET
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item Item)
+        {
+            _context.Items.Add(Item);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            // _context.Items.Add(Id)
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
