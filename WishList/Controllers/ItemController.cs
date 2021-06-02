0702b8f938996d19280d57ebff098db5b282cbb8
Aidan Baker <baker339@purdue.edu>
2021-06-02 14:54:09 -0400

    Updated constructor method on ItemController.cs

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -8,6 +8,11 @@ namespace WishList.Controllers
     public class ItemController : Controller
     {
         private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         
         
         // GET
