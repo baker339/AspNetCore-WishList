09ed840aa367dcc083a6a280ef75f207719c5aa6
Aidan Baker <baker339@purdue.edu>
2021-06-02 13:44:39 -0400

    Added db context

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Item> Items;
+    }
+}
